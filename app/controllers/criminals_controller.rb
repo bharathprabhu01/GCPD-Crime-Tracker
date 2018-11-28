0a77bf1a2a9d76d5f52e9371d12f784f91efc878
Bharath Prabhu <bharathp@andrew.cmu.edu>
2018-11-28 06:48:18 +0000

    done up criminals controller

---
+++ app/controllers/criminals_controller.rb
@@ -0,0 +1,57 @@
+class CriminalsController < ApplicationController
+  before_action :check_login
+  before_action :set_criminal, only: [:show, :edit, :update, :destroy]
+  authorize_resource
+  
+  def index
+    @criminals = Criminal.alphabetical.paginate(page: params[:page]).per_page(10)
+  end
+
+  def new
+    @criminal = Criminal.new
+  end
+
+  def edit
+  end
+
+  def create
+    @criminal = Criminal.new(criminal_params)
+    if @criminal.save
+      redirect_to criminals_path, notice: "Successfully added #{@criminal.name} to GCPD."
+    else
+      render action: 'new'
+    end
+  end
+  
+  def show
+    @cases = @criminal.investigations.alphabetical.to_a
+  end
+
+  def update
+    respond_to do |format|
+      if @criminal.update_attributes(criminal_params)
+        format.html { redirect_to @criminal, notice: "Updated information" }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+  
+  def destroy
+    if @criminal.destroy
+      redirect_to criminals_path, notice: "Successfully removed #{@criminal.name} from GCPD."
+    else
+      @cases = @criminal.investigations.alphabetical.to_a
+      render action: 'show'
+    end
+  end
+ 
+  private
+  def set_criminal
+    @criminal = Criminal.find(params[:id])
+  end
+  
+  def criminal_params
+    params.require(:criminal).permit(:first_name, :last_name, :aka, :convicted_felon, :enhanced_powers, :notes)
+  end
+end
