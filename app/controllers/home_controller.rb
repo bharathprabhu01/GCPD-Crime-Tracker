66a4e89074f15efd51fd3ae0e847710b218e70c5
Bharath Prabhu <bharathp@andrew.cmu.edu>
2018-12-07 22:31:19 +0000

    done up commish dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # commish dashboard
+    @most_wanted_criminals =  Criminal.all.sort_by{|c| c.suspects.current.size}.reverse.take(5)
+    @longest_open_investigations = Investigation.all.is_open.sort_by{|i| i.date_opened - Date.current}.take(5)
+    @open_cases_with_batman = Investigation.is_open.with_batman
   end
 
   def about
